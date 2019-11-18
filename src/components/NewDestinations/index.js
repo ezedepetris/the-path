434c89265ac2b467e9e4247b38772618b0af83b2
Ezequiel Depetris <ezedepetris@gmail.com>
2019-11-18 16:10:08 +1300

    Animate New Destination list component

--- src/components/NewDestinations/index.js
+++ src/components/NewDestinations/index.js
@@ -1,53 +1,102 @@
-import React from 'react';
-import { TextInput, View, Animated, FlatList, Text, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { TextInput, View, Animated, FlatList, Text, TouchableOpacity, Dimensions } from 'react-native';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import GradientButton from '../../components/GradientButton';
 import styles from './styles';
 
-const NewDestinations = ({addresses, addNewDestination, close}) => (
- <View style={styles.container}>
-  <GradientButton
-    text={'Done'}
-    // colors={text === "FOLLOWING" ? ["#a8b6c8", "#a8b6c8"] : ""}
-    // textStyle={textStyle}
-    style={{ alignSelf:'flex-end', marginRight: 10, marginBottom: 5,top:0, paddingVertical: 5}}
-    onPress={close}
-  />
- <View style={styles.flatListContainer}>
+const { height } = Dimensions.get('window');
 
-    <FlatList
-      data={addresses}
-      renderItem={({ item, index }) => renderItem(item, index, addNewDestination)}
-      style={styles.flatList}
-      // contentContainerStyle={styles.flatListContainer}
-      keyExtractor={item => item.placeId}
-    />
-  </View>
-  </View>
-);
+class NewDestinations extends Component {
+  constructor(props) {
+    super(props)
 
-const renderItem = (item, index, addNewDestination) => (
-  <TouchableOpacity style={styles.item} onPress={() => addNewDestination(item)} >
-    <GradientButton
-      text={'+'}
-      // colors={text === "FOLLOWING" ? ["#a8b6c8", "#a8b6c8"] : ""}
-      // textStyle={textStyle}
-      style={{ alignSelf: 'flex-end', marginBottom: 5, paddingVertical: 5}}
-      // onPress={close}
-    />
-    <View style={{ flexDirection: 'column'}}>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.subtitle}>{item.subtitle}</Text>
-    </View>
+    this.state = {
+      addresses: [],
+      visible: false,
+      execAnimation: false
+    }
+  }
 
-  </TouchableOpacity>
-    // <Icon
-    //   name="trash"
-    //   style={styles.icon}
-    // />
-);
+  componentDidMount(){
+    this.setState({ visible: this.props.visible, addresses: this.props.addresses })
+  }
+
+  componentWillReceiveProps(nextProps){
+    let execAnimation = false
+
+    if (nextProps.visible !== this.state.visible)
+      this.setState({ visible: nextProps.visible, execAnimation: true })
+    else
+      this.setState({ execAnimation: false })
+  }
+      
+
+  render() {
+    this.modalAnimation = new Animated.Value(0);
+
+    Animated.timing(this.modalAnimation, {
+      toValue: 1,
+      duration: 600,
+    }).start();
+
+    let outputRange =[]
+    
+    if (!this.state.execAnimation && this.state.visible)
+      outputRange = [0, 0]
+    else if (!this.state.execAnimation)
+      outputRange = [(height * 0.8), (height * 0.8)]
+    else if (this.state.visible)
+      outputRange = [(height * 0.8), 0]
+    else
+      outputRange = [0, (height * 0.8)]
+
+
+    const containerStyle = {
+      transform: [{
+        translateY: this.modalAnimation.interpolate({
+          inputRange: [0, 1],
+          outputRange: outputRange,
+          // outputRange: outputRange,
+        })
+      }],
+    };
+
+    return (
+      <Animated.View style={[containerStyle, styles.container]}>
+        <GradientButton
+          text={'Done'}
+          style={{ alignSelf:'flex-end', marginRight: 10, marginBottom: 5, top:0, paddingVertical: 5}}
+          onPress={this.props.close}
+        />
+       <View style={styles.flatListContainer}>
+          <FlatList
+            data={this.props.addresses}
+            renderItem={({ item, index }) => this.renderItem(item, index)}
+            style={styles.flatList}
+            keyExtractor={item => item.placeId}
+          />
+        </View>
+      </Animated.View>
+    )
+  }
+
+  renderItem (item, index) {
+    return (
+      <TouchableOpacity style={styles.item} onPress={() => this.props.addNewDestination(item)} >
+        <GradientButton
+          text={'+'}
+          style={{ alignSelf: 'flex-end', marginBottom: 5, paddingVertical: 5}}
+        />
+        <View style={{ flexDirection: 'column'}}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default NewDestinations;
 
