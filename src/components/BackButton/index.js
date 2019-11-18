945ce5f2144d177a974950717719f6dc8dfec7bf
Ezequiel Depetris <ezedepetris@gmail.com>
2019-11-18 16:08:36 +1300

    Animate Back Button component

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TextInput, View, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-// import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const BackButton = (props) => {
-  const { onBack, inputStyle } = props;
-  return (
-    <Animated.View style={styles.view}>
-      <Icon
-        name="ios-arrow-round-back"
-        color="#C6C6C6"
-        style={styles.icon}
-        onPress={onBack}
-      />
-    </Animated.View>
-  );
+class BackButton extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      visible: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ visible: this.props.visible })
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextProps.visible != this.state.visible);
+  }
+
+  componentWillReceiveProps(nextProps){
+    if (nextProps.visible !== this.props.visible) {
+      this.setState({ visible: nextProps.visible})
+    }
+  }
+
+  render() {
+    const { onBack, inputStyle, visible } = this.props;
+
+    const backAnimation = new Animated.Value(0);
+    Animated.timing(backAnimation, {
+      toValue: 1,
+      duration: 600,
+    }).start();
+
+    let outputRange = visible ? [-100, 0] : [0, -100]
+
+    const containerStyle = {
+      transform: [{
+          translateX: backAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: outputRange,
+          })
+      }],
+    };
+
+    return (
+      <Animated.View style={[containerStyle, styles.view]}>
+        <Icon
+          name="ios-arrow-round-back"
+          color="#C6C6C6"
+          style={styles.icon}
+          onPress={onBack}
+        />
+      </Animated.View>
+    );
+  }
 };
 
 export default BackButton;
