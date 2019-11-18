03fe7d951f007c41bf1d7bd7ed2388749a16b095
Ezequiel Depetris <ezedepetris@gmail.com>
2019-11-18 16:08:59 +1300

    Animate Destination list component

--- src/components/Destinations/index.js
+++ src/components/Destinations/index.js
@@ -1,44 +1,104 @@
-import React from 'react';
-import { TextInput, View, Animated, FlatList, Text, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { TextInput, View, Animated, FlatList, Text, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const Destinations = ({directions, setDestination, removeDestination}) => (
- <View style={styles.container}>
-    <Text style={styles.header} >Destinations</Text>
-    <SwipeListView
-      useFlatList
-      data={directions}
-      renderItem={({item, index}) => renderItem(item, index, setDestination)}
-      renderHiddenItem={(data, rowMap) => renderLeftItem(data, rowMap, removeDestination)}
-      disableLeftSwipe
-      leftOpenValue={70}
-      style={styles.flatList}
-      keyExtractor={item => item.placeId}
-    />
-  </View>
-);
+const { height } = Dimensions.get('window');
 
-const renderItem = (item, index, setDestination) => (
-  <View style={styles.itemContainer} >
-    <TouchableOpacity style={styles.item} onPress={() => setDestination(item.location) } >
-      <Text style={styles.title}>({index+1}) {item.title}</Text>
-      <Text style={styles.subtitle}>{item.subtitle}</Text>
-    </TouchableOpacity>
-  </View>
-);
+class Destinations extends Component {
 
-const renderLeftItem = (data, rowMap, removeDestination) => (
-  <TouchableOpacity onPress={() => removeDestination(data.item.placeId) } style={styles.iconContainer}>
-    <Icon
-      name="trash"
-      style={styles.icon}
-    />
-  </TouchableOpacity>
-)
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      directions: [],
+      visible: true,
+      execAnimation: false
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ visible: this.props.visible, directions: this.props.directions })
+  }
+
+  componentWillReceiveProps(nextProps){
+    let execAnimation = false
+
+    if (nextProps.visible !== this.state.visible)
+      this.setState({ visible: nextProps.visible, execAnimation: true })
+    else
+      this.setState({ execAnimation: false })
+  }
+
+  render() {
+    this.modalAnimation = new Animated.Value(0);
+
+    Animated.timing(this.modalAnimation, {
+      toValue: 1,
+      duration: 600,
+    }).start();
+
+    let outputRange =[]
+    
+    if (!this.state.execAnimation && this.state.visible)
+      outputRange = [0, 0]
+    else if (!this.state.execAnimation)
+      outputRange = [(height * 0.8), (height * 0.8)]
+    else if (this.state.visible)
+      outputRange = [(height * 0.8), 0]
+    else
+      outputRange = [0, (height * 0.8)]
+
+    const containerStyle = {
+      transform: [{
+        translateY: this.modalAnimation.interpolate({
+          inputRange: [0, 1],
+          outputRange: outputRange,
+        })
+      }],
+    };
+
+    return (
+     <Animated.View style={[containerStyle, styles.container]}>
+        <Text style={styles.header}>Destinations</Text>
+        <SwipeListView
+          useFlatList
+          data={this.props.directions}
+          renderItem={this.renderItem.bind(this)}
+          renderHiddenItem={this.renderLeftItem.bind(this)}
+          disableLeftSwipe
+          leftOpenValue={70}
+          style={styles.flatList}
+          keyExtractor={item => item.placeId}
+        />
+      </Animated.View>
+    );
+  }
+
+  renderItem ({item, index}) {
+    return (
+      <View style={styles.itemContainer} >
+        <TouchableOpacity style={styles.item} onPress={() => this.props.setDestination(item.location) } >
+          <Text style={styles.title}>({index+1}) {item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderLeftItem (data, rowMap) {
+    return (
+      <TouchableOpacity onPress={() => this.props.removeDestination(data.item.placeId) } style={styles.iconContainer}>
+        <Icon
+          name="trash"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    )
+  }
+}
 export default Destinations;
 
 Destinations.defaultProps = {
